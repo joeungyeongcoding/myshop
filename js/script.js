e73774538875de7e049da39d8b80747856fc6e58
joeungyeongcoding <joeungyeong.coding@gmail.com>
2024-02-02 14:48:27 +0900

    2024-02-02 third commit

--- js/script.js
+++ js/script.js
@@ -17,8 +17,19 @@ $(function(){
    $('.listview').click(function(e){
     e.preventDefault();
     const view = $(this).data("view");
-    alert(view);
-
+    $("#pdlist>div").removeClass(); 
+    $(".listview rect").removeClass('list-act-color').addClass('list-color');
+    if(view == 3){
+        $(this).find('rect').removeClass('list-color').addClass('list-act-color');
+        $("#pdlist>disk").addClass("col-md-4 mb-5");     
+    }else if(view == 4){
+        $(this).find('rect').removeClass('list-color').addClass('list-act-color');
+        $("#pdlist>disk").addClass("col-md-3 mb-5");
+    }else if(view == 5){
+        $(this).find('rect').removeClass('list-color').addClass('list-act-color');
+        $("#pdlist>disk").addClass("col-md-55 mb-5");
+    }
+   
    })
 });
 
